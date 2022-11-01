49474b570eeb94c2fdbad43df59006194df80140
tgpplayer <114916420+tgpplayer@users.noreply.github.com>
2022-11-01 19:50:51 +0100

    Trying to reduce lines

--- Game.py
+++ Game.py
@@ -1,13 +1,5 @@
 import random
-
-# Tres personajes clases, gerrero, picaro y mago
-# el jugador puede elegir un personaje o ver las estadisticas del mismo, si elgie la segunda opcion, preguntar cual quiere elegir
-# el ordenador va a elegir un persionaje aleatorio entre esos tres
-# estadisticas: vida, ira (en guerreros), energía (picaro), maná(mago), daño de ataque basico, dos habilidades para cada clase
-# Atacar, defender o ver habilidades cuando se esté en un combate
-# La defensa es un bono temporal para subir la defensa en el mismo turno, ya que cuando cese, la defensa bajara a la enterior
-# El ordenador, cuando sea su turno, decide aleatoriamente si ataca, defiende o hace habilidad. Si hace habilidad, elegirla alatoriamente
-# Condicion de vicotia y derrota y mostrar por consola si ha gandao el ordenador o el jugador
+# La defensa se acaba al final del turno, los puntos de energia mana e ira se reducen
 
 warrior = {
     "life": 100,
@@ -62,7 +54,7 @@ def use_hability(player_ch, AI_ch, hability):
     if player_turn:
         if hability == 1:
             for i in player_ch.keys(): #  We need to work with exactly 1 of 2 habilites that characters have, so we use a 'for' and a counter to work  with the correct hability
-                if counter == 5:
+                if counter == 4:
                     print("Player has used", i)
                     AI_ch["life"] -= player_ch[i]
                     print("Actual AI's life:", AI_ch["life"])
@@ -71,7 +63,7 @@ def use_hability(player_ch, AI_ch, hability):
             
         else:
             for i in player_ch.keys():
-                if counter == 6:
+                if counter == 5:
                     print("Player has used", i)
                     AI_ch["life"] -= player_ch[i]
                     print("Actual AI's life:", AI_ch["life"])
@@ -80,33 +72,26 @@ def use_hability(player_ch, AI_ch, hability):
     else:
         if hability == 1:
             for i in AI_ch.keys():
-                if counter == 5:
+                if counter == 4:
                     print("AI has used", i)
+                    
                     player_ch["life"] -= AI_ch[i]
                     print("Actual Player's life:", player_ch["life"])
                     break
                 counter += 1
             
         else:
-            for i in player_ch.keys():
-                if counter == 6:
+            for i in AI_ch.keys():
+                if counter == 5:
                     print("AI has used", i)
                     player_ch["life"] -= AI_ch[i]
                     print("Actual Player's life:", player_ch["life"])
                 counter += 1
 
 def see_statistics(player_ch):
-    if player_ch == warrior:
-        for key, value in player_ch.items():
-            print("Warrior's", key, "->", value)
-    elif player_ch == killer:
-        for key, value in player_ch.items():
-            print("Killer's", key, "->", value)
-    else:
-        for key, value in player_ch.items():
-            print("Wizard's", key, "->", value)
+    for key, value in player_ch.items():
+        print(player_character, "'s", key, "->", value)
 
-# Characters tuple
 characters = {
     "Warrior": warrior, 
     "Killer": killer,
@@ -137,12 +122,9 @@ def turns(turn):
         use_hability(characters[player_character], characters[IA_character], 2)
     else:
         print("Option not valid. Choose a correct one.")
-        turns()
-    
-    
+        turns(turn)
 
 def battle_flow():
-    # print("BATTLE IS ABOUT TO START!")
     global player_turn
     player_turn = None
     r_number = random.randint(0, 1)
@@ -151,13 +133,20 @@ def battle_flow():
         player_turn = True
     else:
         player_turn = False
-    #player_turn = True # Borrar esto cuando sea necesario
+
     while True:
         turns(player_turn)
-        if player_turn:
+        if player_turn == True:
             player_turn = False
-        else:
+        elif player_turn == False:
             player_turn = True
+        
+        if characters[player_character]["life"] <= 0:
+            print("Player's life is below 0.\nAI WINS!!!")
+            break
+        if characters[IA_character]["life"] <= 0:
+            print("AI's life is below 0.\nPLAYER WINS!!!")
+            break
 
 def initial_action():
     print("Characters to be chosen: Warrior (w), Killer (k), Wizard(z). Choose character or see statistics (ss)...")
@@ -170,9 +159,9 @@ def initial_action():
         "z": "Wizard",
         "ss": "See statistics"
     }
-    option_is_valid = None
-
+    
     # Depending of the option chosen by the player, a boolean takes a value or another
+    option_is_valid = None
     for i in options.keys():
         if initial_player_action != i:
             option_is_valid = False
@@ -183,14 +172,13 @@ def initial_action():
     # If boolean is true, comtinue with question with player and the beggining of the battle, otherwise
     # use reclusion to repeat the process until the user takes a valid choice
     if option_is_valid:
+        global player_character
         if initial_player_action == "ss":
-            print("From which character?")
-            statistics_option = input()
-            see_statistics(characters[options[statistics_option]]) # Use 2 dictionaries in order to get to the statistics
+            print("From which character? (Warrior/Killer/Wizard)")
+            player_character = input()
+            see_statistics(characters[options[player_character]]) # Use 2 dictionaries in order to get to the statistics
             initial_action()
         else:
-            # Player character
-            global player_character
             player_character = options[initial_player_action]
 
             # For IA character, we store the keys of characters in a list for later pick a random one (a character) and assign the character to the AI
@@ -210,4 +198,3 @@ def initial_action():
         initial_action()
 
 initial_action()
-
