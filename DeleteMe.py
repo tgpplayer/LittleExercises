30e7bbb195cea751efd1b901ad3335ac084d26d6
tgpplayer <114916420+tgpplayer@users.noreply.github.com>
2023-11-18 21:37:15 +0100

    Update DeleteMe.py

--- DeleteMe.py
+++ DeleteMe.py
@@ -2,16 +2,8 @@ import pyautogui as pg
 import time
 
 time.sleep(2)
-# txt = open("animal.txt", "r")
+txt = open("animal.txt", "r")
 
-# for i in txt:
-#     pg.write("Usted es " + i)
-#     pg.press('Enter')
-
-for i in range(20):
-    pg.write("Juegas?")
-    time.sleep(0.5)
-    pg.press("Enter")
-
-# pg.write("AY MI GFA")
-# pg.press('Enter')
+for i in txt:
+pg.write("Usted es " + i)
+pg.press('Enter')
